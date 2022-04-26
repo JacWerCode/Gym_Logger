944bf8fbf4877edce737d58db1d949bc3f328558
Jacob <jacobwerschey@gmail.com>
2022-04-26 13:10:51 -0600

    rearrange optins

--- Gym_Logger.py
+++ Gym_Logger.py
@@ -10,18 +10,12 @@ import numpy as np
 #import plotly.graph_objects as go
 #import plotly.express as px
 import streamlit as st
+import datetime as dt
 st.set_page_config(layout="wide")
 #from openpyxl import load_workbook
 #import regex as re
 from collections import defaultdict
 
-
-session_exercises = pd.DataFrame()
-
-
-def append_exercise(session_exercises,data):
-    #st.write(data)
-    pass 
 
 def rearrange(list_object,target):
     return [list_object.pop(list_object.index(target))]+list_object
@@ -32,25 +26,44 @@ def load_exercises():
 
 ref = load_exercises()
 
-fill_in = ['Yeet']
+exercises = sorted(ref.index.unique())
+
+
+session_exercises = pd.DataFrame()
+
+
 
 
 exercise_data = {}
 
 
-
-exercises = sorted(ref.index.unique())
-exercise_data['Exercise'] = st.selectbox('Excersies',exercises)
+person,ex,exercise = st.columns([1,1,6])
 
 
-exercise_data['Weight'] = st.number_input('Weight',0.,1000.,float(ref.loc[exercise_data['Exercise'],'Weight']),5.)
+individuals = ['Nate','Selena','Jacob']
+
+exercise_data['Individual'] = person.selectbox('Individual',individuals)
+
+existing_exercise = ex.selectbox('Exising Exercise',(True,False))
 
 
-exercise_data['Sets'] =st.number_input('Sets',1,10,4,1)
+if existing_exercise:
+    exercise_data['Exercise'] = exercise.selectbox('Exercise',exercises)
+    start_weight = float(ref.loc[exercise_data['Exercise'],'Weight'])
+    start_reps = ref.loc[exercise_data['Exercise'],'Reps']
+else:
+    exercise_data['Exercise'] = exercise.text_input('Exercise')
+    start_weight=50.
+    start_reps = '10-12'
 
-rep_ranges = rearrange(sorted(ref['Reps'].unique()),ref.loc[exercise_data['Exercise'],'Reps'])
-exercise_data['Reps'] = st.selectbox('Rep Range',rep_ranges)
 
+weight, sets, reps = st.columns([1,1,1])
 
-st.button('Log excerise',on_click=append_exercise(session_exercises,exercise_data))
+exercise_data['Weight'] = weight.number_input('Weight',0.,1000.,start_weight,5.)
 
+exercise_data['Sets'] =sets.number_input('Sets',1,10,4,1)
+
+rep_ranges = rearrange(sorted(ref['Reps'].unique()),start_reps)
+exercise_data['Reps'] = reps.selectbox('Rep Range',rep_ranges)
+
+st.write(pd.DataFrame(exercise_data,index=[0]))    
