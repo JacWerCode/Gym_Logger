2a3990718d325c9bdf1370376882ecbfab66a6e1
Jacob <jacobwerschey@gmail.com>
2022-04-28 10:12:00 -0600

    Made summary

--- Gym_Logger.py
+++ Gym_Logger.py
@@ -15,7 +15,6 @@ st.set_page_config(layout="wide")
 #from openpyxl import load_workbook
 #import regex as re
 #from collections import defaultdict
-import os
 
 #import gspread
 from dateutil import tz
@@ -49,7 +48,7 @@ def append_user_exercise(user_df):
 
 user_exists, user = st.columns([1,4])
 #date, time = st.columns([1,1])
-ex,exercise,weight = st.columns([1,3,1])
+ex,exercise,weight,reps,setType = st.columns([1,2,1,1,1])
 
 
 
@@ -68,65 +67,89 @@ ex_select = ex.selectbox('Exercise Source',('Existing Exercise','New Exercise'))
 if ex_select=='Existing Exercise':
     exercise_data['Exercise'] = exercise.selectbox('Exercise',exercises)
     start_weight = float(ref.loc[exercise_data['Exercise'],'Weight'])
-    start_reps = ref.loc[exercise_data['Exercise'],'Reps']
+#    start_reps = ref.loc[exercise_data['Exercise'],'Reps']
 if ex_select == 'New Exercise':
     exercise_data['Exercise'] = exercise.text_input('Exercise',placeholder='Type exercise here')
     start_weight=50.
-    start_reps = '10-12'
-
-
-
+#    start_reps = '10-12'
 
 exercise_data['Weight'] = weight.number_input('Weight',0.,1000.,start_weight,5.)
-
-#exercise_data['Sets'] =sets.number_input('Sets',1,10,4,1)
-
-rep_ranges = rearrange(sorted(ref['Reps'].unique()),start_reps)
-
-
+exercise_data['Reps'] = reps.number_input('Reps',1,50,8,1)
+exercise_data['SetType'] = setType.selectbox('SetType',['Normal','To Failure','Superset',])
 
 curr_time = dt.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz('America/Denver'))
 
 exercise_data['DateTime'] = curr_time
+
 exercise_data['Date']  = curr_time.date() #date.date_input('Day',value = )
 exercise_data['Day'] = exercise_data['Date'].strftime('%A')[:3]
 exercise_data['Time'] = curr_time.time().strftime("%H:%M:%S")
 
 
 
-submit, rest,_ = st.columns([1,1,8])
+submit, rest,_ = st.columns([1,4,5])
 
 if submit.button('Submit Exercise'):
-    
     
     submitted_ex = pd.DataFrame(exercise_data,index=[0])
     
     db = pd.concat([db,submitted_ex],ignore_index=True).reset_index(drop=True)
     db.to_csv('Data/DataBase.csv',index=None)
     db = load_db()
-    
 
-
-if rest.button("Add Rest"):
-    exercise_data['Exercise'] = 'Rest'
+if rest.button(f"Done with {exercise_data['Exercise']}"):
+    exercise_data['Exercise'] = '---'
     exercise_data['Weight'] = 0
     db = pd.concat([db,pd.DataFrame(exercise_data,index=[0])],ignore_index=True).reset_index(drop=True)
     db.to_csv('Data/DataBase.csv',index=None)
     db = load_db()
 
-st.write('Last 5 Exercises')
-user_mask = db['User'] == exercise_data['User']
-
-
-
 if st.button('Clear User History'):
+    user_mask = db['User'] == exercise_data['User']
     db = db[~user_mask]
     db.to_csv('Data/DataBase.csv',index=None)
 
-
-
-    
+#.write('Last 5 Exercises')
+db['Volume'] = db['Weight']*db['Reps']
 
 user_mask = db['User'] == exercise_data['User']
-user_display = db[user_mask].tail().reset_index(drop=True)
-st.write(user_display.drop('DateTime',axis=1).tail())
+user_df = db[user_mask]
+#user_display = user_df.reset_index(drop=True)
+#st.dataframe(user_display.drop('DateTime',axis=1))
+
+user_df['TimeSpent'] = 0
+user_df['SetID'] = 0
+set_id = 0
+for i in user_df.index[1:]:
+    if user_df.loc[i,'Exercise'] !=  user_df.loc[i-1,'Exercise']:
+        set_id+=1
+    user_df.loc[i,'SetID'] = set_id
+    user_df.loc[i-1,'TimeSpent'] = (dt.datetime.fromisoformat(user_df.loc[i,'DateTime']) - dt.datetime.fromisoformat(user_df.loc[i-1,'DateTime'])).seconds
+
+
+user_df = user_df[user_df['Exercise'] != '---']
+
+gby = user_df.groupby(['Date','SetID','Exercise'])['Weight','Reps','Volume','TimeSpent'].agg(['min','max','sum','count']).reset_index()
+gby.columns = [f'{stat}{metric}' for metric, stat in gby.columns]
+
+#gby['maxDateTime'] = gby['maxDateTime'].apply(lambda x: dt.datetime.fromisoformat(x))
+#gby['minDateTime'] = gby['minDateTime'].apply(lambda x: dt.datetime.fromisoformat(x))
+
+if len(gby) > 0:
+
+    summary = pd.DataFrame()
+    summary['Date'] = gby['Date']
+    summary['Exercise'] = gby['Exercise']
+    summary['Weight'] = gby['minWeight'].astype(str) +" - "+ gby['maxWeight'].astype(str)
+    summary['Weight'] = summary['Weight'].apply(lambda x: x.split(" - ")[0] if x.split(" - ")[0]==x.split(" - ")[1] else x)
+    summary['Reps'] = gby['minReps'].astype(str) +" - "+ gby['maxReps'].astype(str)
+    summary['Reps'] = summary['Reps'].apply(lambda x: x.split(" - ")[0] if x.split(" - ")[0]==x.split(" - ")[1] else x)
+    
+    summary['Sets'] = gby['countWeight']
+    summary['TotalReps'] = gby['sumReps'].astype(str)
+    summary['TotalVolume'] = gby['sumVolume'].astype(str)
+    summary['Time'] = gby['sumTimeSpent'].astype(str)
+    
+    #st.dataframe(user_df)
+    #st.dataframe(gby)
+    st.dataframe(summary.tail())
