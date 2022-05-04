6c50d8bbd073aa7d991acd8f9dabc9261027b74e
Jacob <jacobwerschey@gmail.com>
2022-05-04 15:51:25 -0600

    now runs off google sheets

--- Gym_Logger.py
+++ Gym_Logger.py
@@ -8,103 +8,104 @@ Created on Sun Apr 24 13:19:31 2022
 import pandas as pd 
 import streamlit as st
 import datetime as dt
-st.set_page_config(layout="wide")
-
-#import gspread
+st.set_page_config(layout="wide",initial_sidebar_state='collapsed')
+import gspread
 from dateutil import tz
 
-def rearrange(list_object,target):
-    return [list_object.pop(list_object.index(target))]+list_object
 
 @st.cache
 def load_exercises():    
     return  pd.read_csv('NS_Ref.csv').set_index('Exercise')
 
-def load_db():
-    return pd.read_csv('Data/DataBase.csv',parse_dates=True)
+def load_user_data(user):
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    db = gc.open("Gym_Logger").worksheet(user).get_all_values()
+    df = pd.DataFrame(db[1:],columns=db[0])
+    df[['Weight','Reps']] =  df[['Weight','Reps']].astype('float')
+    return df
+
+def update_sheet(user,df):
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    worksheet = gc.open("Gym_Logger").worksheet(user)
+    submitted_ex = pd.DataFrame(exercise_data,index=[0]) 
+    df = pd.concat([df,submitted_ex],ignore_index=True).reset_index(drop=True)
+    #st.write(df.columns.values.tolist())
+    #st.write(df.values.tolist())
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    return df
 
 ref = load_exercises().reset_index()
-db = load_db()
-
-#main_link = 'https://docs.google.com/spreadsheets/d/1pVvJNRYqnsJVFHqFLNMehmCsQxqQagE_MKiH5Ue9T9c/edit?usp=sharing'
-
 exercise_data = {}
-
 user = st.columns([1])[0]
-ex_type,exercise,exercise2,weight,reps,setType = st.columns([1,2,2,1,1,1])
+ex_type,exercise,weight,reps = st.columns([1,2,1,1])
 notes = st.columns([1])[0]
 
+sidebar = st.sidebar
+sidebar.header('New User')
+new_user = sidebar.text_input('New User',placeholder='Type Username Here')
+if sidebar.button("Add New User"):
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    col_names = ['User','Date','Time','MuscleGroup','Exercise','Weight','Reps','Notes','DateTime']
+    gc.open("Gym_Logger").add_worksheet(title=new_user,rows=1,cols=len(col_names))
+    gc.open("Gym_Logger").worksheet(new_user).update('A1:I1', [col_names])
+    
+def add_new():
+    sidebar.header('New Exercise')
+    muscle_group = sidebar.text_input('Muscle Group',placeholder='Type Muscle Group Here')
+    new_exercise = sidebar.text_input('New Exercise',placeholder='Type Exercise Here')
+    #push_pull = sidebar.select_box('Push/Pull',placeholder='Push or Pull')
+    if sidebar.button("Add Exercise"):
+        pass
+  
 
-individuals = ['Nate','Selena','Jacob']
-exercise_data['User'] = user.selectbox('User',individuals)
+    
 
+worksheets = gspread.service_account(filename='cashsheets2.json').open("Gym_Logger").worksheets()
+users = sorted(ws.title for ws in worksheets)
+exercise_data['User'] = user.selectbox('User',users)
+df = load_user_data(exercise_data['User'])
 
 ex_type_value = ex_type.selectbox('Muscle Group',sorted(ref['Body'].unique()))
-
 body_ref = ref[ref['Body'] == ex_type_value]
+ex_selection = exercise.selectbox('Exercise Selection',sorted(body_ref['Exercise']))
 
 
-ex_selection = exercise.selectbox('Exercise Selection',sorted(body_ref['Exercise'])+['Other'])
 
-exercise_data['Body Part'] = ex_type_value
-
-if ex_selection == 'Other':
-    exercise_data['Exercise'] = exercise2.text_input('Exercise',placeholder='Type Exercise Here')
-else:
-    exercise_data['Exercise'] = ex_selection
-
- 
-
-
-exercise_data['Weight'] = weight.number_input('Weight',0.,1000.,50.0,2.5)
+exercise_data['MuscleGroup'] = ex_type_value
+exercise_data['Exercise'] = ex_selection
+exercise_data['Weight'] = weight.number_input('Weight',0.,1000.,50.0,5.)
 exercise_data['Reps'] = reps.number_input('Reps',1,50,8,1)
-exercise_data['SetType'] = setType.selectbox('SetType',['Normal','To Failure','Superset',])
 
 curr_time = dt.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz('America/Denver'))
 
-exercise_data['DateTime'] = curr_time
+exercise_data['DateTime'] = str(curr_time)
 
-exercise_data['Date']  = curr_time.date() #date.date_input('Day',value = )
-exercise_data['Day'] = exercise_data['Date'].strftime('%A')[:3]
+exercise_data['Date']  = str(curr_time.date()) #date.date_input('Day',value = )
+#exercise_data['Day'] = curr_time.strftime('%A')[:3]
 exercise_data['Time'] = curr_time.time().strftime("%H:%M:%S")
 exercise_data['Notes'] = notes.text_input('Notes',placeholder='Type Notes here')
 
 
+#st.write(exercise_data)
+
 submit, rest,_ = st.columns([1,3,5])
 
 if submit.button('Submit'):
-    
-    submitted_ex = pd.DataFrame(exercise_data,index=[0])
-    
-    db = pd.concat([db,submitted_ex],ignore_index=True).reset_index(drop=True)
-    db.to_csv('Data/DataBase.csv',index=None)
-    db = load_db()
+    df = update_sheet(exercise_data['User'],df)
 
 if rest.button(f"Done with {exercise_data['Exercise']}"):
     exercise_data['Exercise'] = '---'
     exercise_data['Weight'] = 0
-    db = pd.concat([db,pd.DataFrame(exercise_data,index=[0])],ignore_index=True).reset_index(drop=True)
-    db.to_csv('Data/DataBase.csv',index=None)
-    db = load_db()
 
-def clear_history_button(db,exercise_data):
-    if st.button('Clear User History'):
-        user_mask = db['User'] == exercise_data['User']
-        db = db[~user_mask]
-        db.to_csv('Data/DataBase.csv',index=None)
-    return db
+    df = update_sheet(exercise_data['User'],df)
 
-db = clear_history_button(db,exercise_data)
+df['Volume'] = df['Weight']*df['Reps']
 
 
+df = df[df['Exercise'] != '---']
 
-db['Volume'] = db['Weight']*db['Reps']
 
-user_mask = db['User'] == exercise_data['User']
-user_df = db[user_mask].reset_index(drop=True)
-user_df = user_df[user_df['Exercise'] != '---']
-
-gby = user_df.groupby(['Date','Exercise'])[['Weight','Reps','Volume','Time']].agg(['min','max','sum','count']).reset_index()
+gby = df.groupby(['Date','Exercise'])[['Weight','Reps','Volume','Time']].agg(['min','max','sum','count']).reset_index()
 gby.columns = [f'{stat}{metric}' for metric, stat in gby.columns]
 
 show_n = 5
@@ -124,8 +125,7 @@ summary['Sets'] = gby['countWeight']
 summary['TotalReps'] = gby['sumReps'].astype(str)
 summary['TotalVolume'] = gby['sumVolume'].astype(str)
 
-#st.dataframe(user_df)
+#st.dataframe(df)
 #st.dataframe(gby)
-
 summary = summary.sort_values('LastRepTime').drop(['FirstRepTime','LastRepTime'],axis=1)
 st.write(summary.tail(show_n))
