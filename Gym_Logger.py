5f2f33ea973d1d1bbeaa47e8233d077e075cdeab
Jacob <jacobwerschey@gmail.com>
2022-05-04 16:39:20 -0600

    added sidebar to add new users and exercises

--- Gym_Logger.py
+++ Gym_Logger.py
@@ -12,10 +12,10 @@ st.set_page_config(layout="wide",initial_sidebar_state='collapsed')
 import gspread
 from dateutil import tz
 
-
-@st.cache
-def load_exercises():    
-    return  pd.read_csv('NS_Ref.csv').set_index('Exercise')
+def load_exercises():
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    db = gc.open("Gym_Logger_Ex").worksheet("Exercises").get_all_values()
+    return  pd.DataFrame(db[1:],columns=db[0])
 
 def load_user_data(user):
     gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
@@ -29,19 +29,16 @@ def update_sheet(user,df):
     worksheet = gc.open("Gym_Logger").worksheet(user)
     submitted_ex = pd.DataFrame(exercise_data,index=[0]) 
     df = pd.concat([df,submitted_ex],ignore_index=True).reset_index(drop=True)
-    #st.write(df.columns.values.tolist())
-    #st.write(df.values.tolist())
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
     return df
 
-ref = load_exercises().reset_index()
+ref = load_exercises().reset_index(drop=True)
 exercise_data = {}
 user = st.columns([1])[0]
 ex_type,exercise,weight,reps = st.columns([1,2,1,1])
 notes = st.columns([1])[0]
 
 sidebar = st.sidebar
-sidebar.header('New User')
 new_user = sidebar.text_input('New User',placeholder='Type Username Here')
 if sidebar.button("Add New User"):
     gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
@@ -49,25 +46,29 @@ if sidebar.button("Add New User"):
     gc.open("Gym_Logger").add_worksheet(title=new_user,rows=1,cols=len(col_names))
     gc.open("Gym_Logger").worksheet(new_user).update('A1:I1', [col_names])
     
-def add_new():
-    sidebar.header('New Exercise')
-    muscle_group = sidebar.text_input('Muscle Group',placeholder='Type Muscle Group Here')
-    new_exercise = sidebar.text_input('New Exercise',placeholder='Type Exercise Here')
-    #push_pull = sidebar.select_box('Push/Pull',placeholder='Push or Pull')
-    if sidebar.button("Add Exercise"):
-        pass
-  
 
-    
+sidebar.header('New Exercise')
+new_ex = {}
+new_ex['MuscleGroup'] = sidebar.text_input('Muscle Group',placeholder='Type Muscle Group Here')
+new_ex['Exercise'] = sidebar.text_input('New Exercise',placeholder='Type Exercise Here')
+new_ex['PP']  = 'TBA'
+if sidebar.button("Add Exercise"):
+    r = pd.DataFrame(new_ex,index = [0])
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    worksheet = gc.open("Gym_Logger_Ex").worksheet("Exercises")
+    ref = pd.concat([ref,r],ignore_index=True)
+    worksheet.update([ref.columns.values.tolist()] + ref.values.tolist())
+  
 
 worksheets = gspread.service_account_from_dict(st.secrets["gcp_service_account"]).open("Gym_Logger").worksheets()
 users = sorted(ws.title for ws in worksheets)
 exercise_data['User'] = user.selectbox('User',users)
 df = load_user_data(exercise_data['User'])
 
-ex_type_value = ex_type.selectbox('Muscle Group',sorted(ref['Body'].unique()))
-body_ref = ref[ref['Body'] == ex_type_value]
+ex_type_value = ex_type.selectbox('Muscle Group',sorted(ref['MuscleGroup'].unique()))
+body_ref = ref[ref['MuscleGroup'] == ex_type_value]
 ex_selection = exercise.selectbox('Exercise Selection',sorted(body_ref['Exercise']))
+
 
 
 
@@ -93,14 +94,17 @@ submit, rest,_ = st.columns([1,3,5])
 if submit.button('Submit'):
     df = update_sheet(exercise_data['User'],df)
 
-if rest.button(f"Done with {exercise_data['Exercise']}"):
-    exercise_data['Exercise'] = '---'
-    exercise_data['Weight'] = 0
+def done_with(df):
+    if rest.button(f"Done with {exercise_data['Exercise']}"):
+        exercise_data['Exercise'] = '---'
+        exercise_data['Weight'] = 0
+        df = update_sheet(exercise_data['User'],df)
 
-    df = update_sheet(exercise_data['User'],df)
+
+#done_with(df)
+
 
 df['Volume'] = df['Weight']*df['Reps']
-
 
 df = df[df['Exercise'] != '---']
 
@@ -109,7 +113,7 @@ gby = df.groupby(['Date','Exercise'])[['Weight','Reps','Volume','Time']].agg(['m
 gby.columns = [f'{stat}{metric}' for metric, stat in gby.columns]
 
 show_n = 5
-st.header(f'Last {show_n} exercises')
+st.header(f'Summary of last {show_n} exercises')
 
 summary = pd.DataFrame()
 summary['Date'] = gby['Date']
